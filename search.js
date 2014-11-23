12d7a36ab2c325553f156bd0c5ca2c091b88401b
Alexander Yang <liming.alexander@yahoo.com>
2014-11-23 15:47:21 -0800

    major webpage update
    -added a login page
    -added a checkout page
    
    -broke the original page into a search page and a ticket find page
    -general aesthetic changes

--- search.js
+++ search.js
@@ -23,8 +23,20 @@ function searchVenue() {
 	});
 }
 
-function gotoSeatMap() {
-	window.location.href="/seatmap.html?" + eventId;	
+function goHome() {
+	window.location.href="index.html";	
+}
+
+function goVenue(id) {
+	window.location.href="ticketsearch.html";
+}
+
+function goLogin() {
+	window.location.href="login.html";
+}
+
+function goCheckout() {
+	window.location.href="checkout.html";
 }
 
 function findTickets() {
@@ -39,5 +51,48 @@ function findTickets() {
 	}, function(resp) {
 		// redirect to ticket listing page
 		window.location.href="/ticketlist.html?ids";
+	});
+}
+
+var updateObj;
+function StartTimer(seconds) {
+	var time = new Date();
+	var startTime = time.getTime();	
+	
+	if (updateObj) {
+		window.clearInterval(updateObj);
 	}
+	updateObj = window.setInterval(function () {updateTime(startTime, seconds * 1000)}, 500);
+	
+	$(".timer").show();
+}
+
+function updateTime(startTime, waitTime) {
+	var date = new Date();
+	var currTime = date.getTime();
+	if (currTime >= startTime + waitTime) {
+		window.clearInterval(updateObj);
+	}
+	else {
+		var timer = document.getElementById("time");
+		if (timer) {
+			var timeLeft = new Date(startTime + waitTime - currTime);
+			timer.innerHTML = convertDate(timeLeft);
+		}
+	}
+}
+
+function convertDate(date) {
+	var timeString;
+	
+	timeString = date.getMinutes() + ":";
+	
+	var seconds = date.getSeconds();
+	if (seconds < 10) {
+		timeString += "0" + seconds;
+	}
+	else {
+		timeString += seconds;
+	}
+	return timeString;
 }
